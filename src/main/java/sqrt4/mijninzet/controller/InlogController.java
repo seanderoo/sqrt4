96de15398a0968cfaf0d81cb3e294670cbe54d7c
Karin Zuidgeest <karin.zuidgeest@ziggo.nl>
2019-05-07 15:17:56 +0200

    omzeilen Spring Security inlogscherm

--- src/main/java/sqrt4/mijninzet/controller/InlogController.java
+++ src/main/java/sqrt4/mijninzet/controller/InlogController.java
@@ -1,7 +1,16 @@
 package sqrt4.mijninzet.controller;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class InlogController extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/index");
+    }
 }
