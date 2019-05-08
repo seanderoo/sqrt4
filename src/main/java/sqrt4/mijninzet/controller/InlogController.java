d9f60aead9c1168f5c67e60009f92bfbc1d45b97
Karin Zuidgeest <karin.zuidgeest@ziggo.nl>
2019-05-08 11:47:19 +0200

    inlogscherm

--- src/main/java/sqrt4/mijninzet/controller/InlogController.java
+++ src/main/java/sqrt4/mijninzet/controller/InlogController.java
@@ -3,6 +3,7 @@ package sqrt4.mijninzet.controller;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InlogController extends WebSecurityConfigurerAdapter {
@@ -12,5 +13,14 @@ public class InlogController extends WebSecurityConfigurerAdapter {
         http
                 .formLogin()
                 .loginPage("/index");
+    }
+
+    @GetMapping("/inlog")
+    public String inlog() {
+        return "inlog";
+    }
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 }
