4063f8a8be0681585bdc86847f1f7ba709cd35fb
Matthijs Verkaaik <matthijsverkaaik@gmail.com>
2019-05-08 10:22:34 +0200

    Application properties MySQL instellingen

--- src/main/java/sqrt4/mijninzet/MijninzetApplication.java
+++ src/main/java/sqrt4/mijninzet/MijninzetApplication.java
@@ -2,6 +2,7 @@ package sqrt4.mijninzet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sqrt4.mijninzet.model.Users.Docent;
 
 @SpringBootApplication
 public class MijninzetApplication {
@@ -9,5 +10,7 @@ public class MijninzetApplication {
     public static void main(String[] args) {
         SpringApplication.run(MijninzetApplication.class, args);
     }
+
+
 
 }
