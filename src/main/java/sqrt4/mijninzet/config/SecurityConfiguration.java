2cddd3ce9830f04745d0346de74df435fcd270d7
Matthijs Verkaaik <matthijsverkaaik@gmail.com>
2019-05-13 14:38:00 +0200

    Spring security eerste versie

--- src/main/java/sqrt4/mijninzet/config/SecurityConfiguration.java
+++ src/main/java/sqrt4/mijninzet/config/SecurityConfiguration.java
@@ -1,21 +1,35 @@
-//package sqrt4.mijninzet.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
-//
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().permitAll();
-//    }
-//}
+package sqrt4.mijninzet.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/voorkeuren")    //Pagina zichtbaar na succesvol inloggen
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER");
+    }
+}
--- src/main/java/sqrt4/mijninzet/controller/InlogController.java
+++ src/main/java/sqrt4/mijninzet/controller/InlogController.java
@@ -5,29 +5,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import sqrt4.mijninzet.repository.UserRepository;
 import sqrt4.mijninzet.model.users.User;
 
 @Controller
-public class InlogController extends WebSecurityConfigurerAdapter {
+public class InlogController {
 
-    @Autowired
-    UserRepository repo;
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .formLogin()
-                .loginPage("/index");
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "/login";
     }
 
-    @GetMapping("/inlog")
-    public String inlog() {
-        return "inlog";
-    }
-    @GetMapping("/test")
-    public String test() {
-        User user = repo.getOne(1); //code om database te intialiseren kan later weg
-        return "test";
-    }
 }
