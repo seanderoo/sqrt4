fbfdc956b0cc563cf7e127ffc705c61866379d0c
Karin Zuidgeest <karin.zuidgeest@ziggo.nl>
2019-05-14 09:33:12 +0200

    login ipv inlog

--- src/main/java/sqrt4/mijninzet/controller/InlogController.java
+++ src/main/java/sqrt4/mijninzet/controller/InlogController.java
@@ -1,36 +1,14 @@
 package sqrt4.mijninzet.controller;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import sqrt4.mijninzet.repository.UserRepository;
-import sqrt4.mijninzet.model.users.User;
 
 @Controller
-public class InlogController extends WebSecurityConfigurerAdapter {
+public class InlogController {
 
-    @Autowired
-    UserRepository repo;
-    private SessionFactory sessionFactory;
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .formLogin()
-                .loginPage("/index");
-    }
-
-    @GetMapping("/inlog")
+    @GetMapping("/login")
     public String inlog() {
-        return "inlog";
-    }
-    @GetMapping("/test")
-    public String test() {
-        User user = repo.getOne(1); //code om database te intialiseren kan later weg
-        return "test";
+        return "login";
     }
 }
