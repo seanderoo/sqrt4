7d7f4dc4f85b06b256c2e607b0c74b558ca7203b
Floris van Andel <andelfc@hva.nl>
2019-05-14 09:43:36 +0200

    Semester entity uitgeschakeld

--- src/main/java/sqrt4/mijninzet/model/Beschikbaarheid/Semester.java
+++ src/main/java/sqrt4/mijninzet/model/Beschikbaarheid/Semester.java
@@ -5,13 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.ArrayList;
 
-@Entity
+
 public class Semester {
     private final int REG_JAAR = 52;
     private final int HALF_JAAR = 26;
     private final int SPEC_JAAR = 53;
     private final int [] SPECIAAL_JAREN = {2015,2020,2026,2032,2037,2043,2048,2054};
-    @Id
+
     private String semesterNaam;
     private int startWeek;
     private int startJaar;
