fc85d80caec8b3368126da41f2d2f3246e3641b1
Seán de Roo <sean.de.roo@hva.nl>
2019-05-16 11:21:17 +0200

    random commit

--- src/main/java/sqrt4/mijninzet/controller/VoorkeurController.java
+++ src/main/java/sqrt4/mijninzet/controller/VoorkeurController.java
@@ -6,11 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.tags.form.RadioButtonTag;
 import sqrt4.mijninzet.model.User;
 import sqrt4.mijninzet.model.Vak;
 import sqrt4.mijninzet.model.Voorkeur;
 import sqrt4.mijninzet.repository.VakRepository;
 import sqrt4.mijninzet.repository.VoorkeurenRepository;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,18 +32,17 @@ public class VoorkeurController {
     }
 
     @PostMapping("/voorkeuren")
-    public String voorkeurToegevoegd(@ModelAttribute("voorkeur") Voorkeur voorkeur, Vak vak, Model model) {
+    public String voorkeurToegevoegd(@ModelAttribute("voorkeur") Voorkeur voorkeur, Vak vak, User user, Model model) {
+
         model.addAttribute("voorkeurId", voorkeur.getVoorkeurId());
-        model.addAttribute("user", voorkeur.getUser());
+        model.addAttribute("user", user.getId());
         model.addAttribute("vak", vak.getVakId());
         model.addAttribute("voorkeur", voorkeur.getVoorkeur());
-        System.out.println("ik ben hier");
-        System.out.println();
-        System.out.println("hierboven staat een vaknaam");
-        System.out.println(voorkeur.getUser());
-        System.out.println("hierboven staat een user");
-        System.out.println(voorkeur.getVoorkeur());
-        System.out.println("hierboven staat mijn voorkeur");
+
+
+        System.out.println("Vak: " + vak.getVakId());
+        System.out.println("User: " + user.getId());
+        System.out.println("Voorkeur: " + voorkeur.getVoorkeur());
         voorkeurenRepository.save(voorkeur);
         return "voorkeuren-toegevoegd";
     }
