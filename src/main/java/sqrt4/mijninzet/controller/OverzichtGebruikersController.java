8a15b416213385c44a59d4481fe83309e3469714
Matthijs Verkaaik <matthijsverkaaik@gmail.com>
2019-05-27 14:02:16 +0200

    Verwijder gebruiker functie toegevoegd

---
+++ src/main/java/sqrt4/mijninzet/controller/OverzichtGebruikersController.java
@@ -0,0 +1,47 @@
+package sqrt4.mijninzet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sqrt4.mijninzet.model.User;
+import sqrt4.mijninzet.repository.UserRepository;
+
+import java.util.List;
+
+@Controller
+public class OverzichtGebruikersController extends AbstractController{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/admin/overzicht-gebruikers")
+    public String overzichtGebruikers(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("gebruikers", users);
+        return "overzicht-gebruikers";
+    }
+
+    @PostMapping("/admin/overzicht-gebruikers")
+    public String verwijderGebruiker( @ModelAttribute("gebruiker") User user, @RequestParam("Verwijder") Long userId, Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("gebruikers", users);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userName = auth.getName();
+        User activeUser = userRepository.findByUsername(userName);
+        
+        if(userId != activeUser.getId()){
+            return "overzicht-gebruikers";
+        }
+        else {
+            userRepository.deleteById(userId);
+        }
+        return "overzicht-gebruikers";
+    }
+}
