4477291ca8a446bdce328cf4c51f74088a9d1840
James <james.tergau@hva.nl>
2019-05-28 10:02:51 +0200

    Vak aanmaken en opslaan in database

--- src/main/java/sqrt4/mijninzet/controller/VakController.java
+++ src/main/java/sqrt4/mijninzet/controller/VakController.java
@@ -3,6 +3,9 @@ package sqrt4.mijninzet.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import sqrt4.mijninzet.model.Vak;
 import sqrt4.mijninzet.repository.VakRepository;
 
 @Controller
@@ -13,8 +16,13 @@ public class VakController {
 
     @GetMapping("/manager/vak-aanmaken")
     public String vakAanmaken() {
-        return "vak-aanmaken";
+        return "/vak-aanmaken";
     }
+
+    @PostMapping("/manager/vak-toegevoegd")
+    public String vakToegevoegd(@ModelAttribute Vak vak){
+        vakRepository.save(vak);
+        return "/vak-toegevoegd";}
 
     @GetMapping("/manager/vak-rooster")
     public String vakRoosterMaken() {
