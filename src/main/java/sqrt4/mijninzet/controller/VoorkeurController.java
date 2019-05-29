ae4e6277187d2f654633030220ef131a585d997f
Seán de Roo <sean.de.roo@hva.nl>
2019-05-29 13:55:56 +0200

    de juiste versie

--- src/main/java/sqrt4/mijninzet/controller/VoorkeurController.java
+++ src/main/java/sqrt4/mijninzet/controller/VoorkeurController.java
@@ -6,13 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import sqrt4.mijninzet.model.User;
 import sqrt4.mijninzet.model.Vak;
 import sqrt4.mijninzet.model.Voorkeur;
 import sqrt4.mijninzet.repository.VakRepository;
 import sqrt4.mijninzet.repository.VoorkeurenRepository;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class VoorkeurController extends AbstractController {
@@ -22,7 +23,7 @@ public class VoorkeurController extends AbstractController {
     @Autowired
     private VoorkeurenRepository voorkeurenRepository;
 
-    @GetMapping("/voorkeuren")
+    @GetMapping("/docent/voorkeuren")
     public String getVakken(Model model) {
         List<Vak> vakkenLijst = vakRepository.findAll();
         User user = userRepo.findByUsername(voegActiveUserToe().getUsername());
@@ -45,7 +46,7 @@ public class VoorkeurController extends AbstractController {
         return "voorkeuren";
     }
 
-    @PostMapping("/voorkeuren")
+    @PostMapping("/docent/voorkeuren")
     public String bewaarVoorkeur(@ModelAttribute("voorkeur") Voorkeur voorkeur, Vak vak, Model model) {
         Voorkeur ingevuldeVoorkeur = new Voorkeur();
         ingevuldeVoorkeur.setUser(voegActiveUserToe());
@@ -56,9 +57,28 @@ public class VoorkeurController extends AbstractController {
 
         List<Vak> vakkenLijst = vakRepository.findAll();
         model.addAttribute("vakkenLijst", vakkenLijst);
-//        model.addAttribute("voorkeur", ingevuldeVoorkeur);
         return "voorkeuren";
     }
 }
 
-
+//    List<Vak> vakken = vakRepository.findAll(); // pas aan voorkeurRepo
+//
+//        for (Vak vak: vakken) {
+//                String key = null;
+//                for (String k : allParams.keySet()) {
+//                int sleutel = Integer.parseInt(k);
+//                if (vak.getVakId() == sleutel) {
+//                key = k;
+//                }
+//                }
+//                Voorkeur voorkeur1 = new Voorkeur();
+//                voorkeur1.setVak(vak);
+//                voorkeur1.setVoorkeur(allParams.get(key));
+//                voorkeur1.setUser(voegActiveUserToe());
+//
+//                if (allParams.get(key) != null) {
+//                voorkeurenRepository.deleteByVak_VakIdAndUser(vak.getVakId(), voegActiveUserToe());
+//                voorkeurenRepository.save(voorkeur1);
+//                }
+//                }
+//                return "voorkeuren";
