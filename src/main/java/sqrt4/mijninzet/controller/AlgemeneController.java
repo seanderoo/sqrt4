0d15cbf24bd0646ed8129380e67ba0e7ffff9521
Seán de Roo <sean.de.roo@hva.nl>
2019-05-29 16:30:01 +0200

    voorkeuren is bijna ajaxproof, voorkeuren worden wel gelijk teruggegeven in de console, nu alleen nog Js schrijven dat het op het scherm wordt teruggegeven

--- src/main/java/sqrt4/mijninzet/controller/AlgemeneController.java
+++ src/main/java/sqrt4/mijninzet/controller/AlgemeneController.java
@@ -1,17 +1,30 @@
 package sqrt4.mijninzet.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import sqrt4.mijninzet.model.User;
+import sqrt4.mijninzet.model.Vak;
+import sqrt4.mijninzet.model.Voorkeur;
+import sqrt4.mijninzet.repository.VoorkeurenRepository;
 
 @RestController
-@RequestMapping("/voorkeuren")
+@RequestMapping("/ajax/voorkeuren")
 public class AlgemeneController {
 
-    @RequestMapping(value="/{user}/{vak}")
-    public String testRest(@PathVariable String user, String vak) {
+    @Autowired
+    private VoorkeurenRepository voorkeurenRepository;
 
-        return "mies";
+    @RequestMapping(value="/{user}/{vak}", method = RequestMethod.POST)
+    public String testRest(@PathVariable User user, @PathVariable Vak vak) {
+        System.out.println("******* Hij doet het!!! User = " + user.getId() + ", vak = " + vak.getVakId());
+        Voorkeur voorkeur = voorkeurenRepository.findVoorkeurByVakAndUser(vak, user);
+        if (voorkeur == null) {
+            System.out.println("deze gebruiker heeft nog geen voorkeur opgegeven voor dit vak :{");
+        } else {
+            System.out.println("dit is het voorkeur voor dit vak van deze gebruiker " + voorkeur.getVoorkeurGebruiker());
+        }
+        return "hij doet het!";
     }
 
 
