7f58d32355a685b9ad748ec0a9c95b9af28bb8e3
Floris van Andel <andelfc@hva.nl>
2019-05-29 15:35:48 +0200

    Incidentregistratie inclusief overzicht zonder verwijderen

--- src/main/java/sqrt4/mijninzet/repository/IncidentregistratieRepository.java
+++ src/main/java/sqrt4/mijninzet/repository/IncidentregistratieRepository.java
@@ -1,5 +1,6 @@
 package sqrt4.mijninzet.repository;
 
+import org.apache.tomcat.jni.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sqrt4.mijninzet.model.Incident;
 import sqrt4.mijninzet.model.User;
@@ -8,10 +9,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 
-public interface IncidentregistratieRepository extends JpaRepository<Incident, Long> {
+public interface IncidentregistratieRepository extends JpaRepository<Incident, LocalDate> {
 
     List<Incident> findAllByUser(User user);
 
-    List<Incident> deleteByDatum(LocalDate date);
+    Incident findByDatum(LocalDate date);
+
+    Incident deleteByDatum(LocalDate date);
 
 }
