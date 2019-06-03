71e80e28dd5f7b5eccd80c0637a9f1761bf096f8
Floris van Andel <andelfc@hva.nl>
2019-06-03 14:40:27 +0200

    FLoris vacatures verwijderen aangemaakt

--- src/main/java/sqrt4/mijninzet/controller/VacatureController.java
+++ src/main/java/sqrt4/mijninzet/controller/VacatureController.java
@@ -2,12 +2,12 @@ package sqrt4.mijninzet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import sqrt4.mijninzet.model.Vacature;
 import sqrt4.mijninzet.repository.VacatureRepository;
+
+import java.util.List;
 
 @RequestMapping("/admin")
 @Controller
@@ -20,6 +20,22 @@ public class VacatureController extends AbstractController{
     public String nieuweVacature(){
     return "/admin/nieuwe-vacature";
     }
+
+    @GetMapping("/overzicht-vacatures")
+    public String overzichtVacatures(Model model){
+        List<Vacature> vacaturelijst = vacrepo.findAll();
+        model.addAttribute("vacatureLijst", vacaturelijst);
+        return "/admin/overzicht-vacatures";
+    }
+
+    @PostMapping("/overzicht-vacatures")
+    public String verwijderVacature(@RequestParam("Verwijder") int id, Model model){
+        vacrepo.deleteById(id);
+        List<Vacature> vacaturelijst = vacrepo.findAll();
+        model.addAttribute("vacatureLijst", vacaturelijst);
+        return "/admin/overzicht-vacatures";
+    }
+
 
     @PostMapping("/vacature-toegevoegd")
     public String nieuweGebruiker(@ModelAttribute("vacature") Vacature vacature) {
