3d83ae3ef741499ded5d727a941833102862ac26
Seán de Roo <sean.de.roo@hva.nl>
2019-06-13 16:52:51 +0200

    @POSTMAPPING koppeldocenten work in progress

--- src/main/java/sqrt4/mijninzet/controller/KoppelDocentenController.java
+++ src/main/java/sqrt4/mijninzet/controller/KoppelDocentenController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import sqrt4.mijninzet.model.Beschikbaarheid.Cohort;
+import sqrt4.mijninzet.model.Beschikbaarheid.Dag;
 import sqrt4.mijninzet.model.Beschikbaarheid.Week;
 import sqrt4.mijninzet.model.User;
 import sqrt4.mijninzet.repository.*;
@@ -23,6 +25,8 @@ public class KoppelDocentenController {
     private CohortRepository cohortRepository;
     @Autowired
     private WeekRepository weekRepository;
+    @Autowired
+    private DagRepository dagRepository;
 
     @GetMapping("roosteraar/docenten-koppelen-kies-cohort")
     public String koppelDocenten(Model model) {
@@ -34,30 +38,99 @@ public class KoppelDocentenController {
 
         List<Cohort> cohortList = cohortRepository.findAllByStartJaarIsGreaterThanEqual(huidigeJaar);
         Predicate<Cohort> condition = cohort -> cohort.getStartJaar() == huidigeJaar && cohort.getStartWeek() <= huidigeWeek;
-        cohortList.removeIf(condition);
+//        cohortList.removeIf(condition);
 
         model.addAttribute("cohortList", cohortList);
         return "roosteraar/docenten-koppelen-kies-cohort";
     }
 
     @GetMapping("roosteraar/docenten-koppelen-gekozen-cohort")
-    public String docentenKoppelen(@RequestParam("cohortNaam") String cohortnaam,
-                                   Model model) {
-
+    public String docentenKoppelen(@RequestParam("cohortNaam") String cohortnaam, Model model) {
         Cohort cohort = cohortRepository.findByCohortNaam(cohortnaam);
         model.addAttribute("cohort", cohort);
-
-        int index = 0;
-        model.addAttribute("index", index);
-
-//        List<Week> weekList = cohort.getWeekList();
-//        model.addAttribute("weekList", weekList);
 
         List<Week> weken = weekRepository.findWeeksByCohortId(cohort.getId());
         model.addAttribute("weken", weken);
 
         List<User> docentList = userRepository.findAllByRolesContaining("DOCENT");
         model.addAttribute("docentList", docentList);
+        return "roosteraar/docenten-koppelen-gekozen-cohort";
+    }
+
+    @PostMapping("roosteraar/docenten-koppelen-gekozen-cohort")
+    public String slaWeekOp(@RequestParam("cohortNaam") String cohortnaam,
+                            @RequestParam(value = "week", required = false) int weekId,
+                            @RequestParam("maOcht") long maOchtDoc,
+                            @RequestParam("diOcht") long diOchtDoc,
+                            @RequestParam("woOcht") long woOchtDoc,
+                            @RequestParam("doOcht") long doOchtDoc,
+                            @RequestParam("vrOcht") long vrOchtDoc,
+                            @RequestParam("maMid") long maMidDoc,
+                            @RequestParam("diMid") long diMidDoc,
+                            @RequestParam("woMid") long woMidDoc,
+                            @RequestParam("doMid") long doMidDoc,
+                            @RequestParam("vrMid") long vrMidDoc,
+                            @RequestParam("maAvo") long maAvoDoc,
+                            @RequestParam("diAvo") long diAvoDoc,
+                            @RequestParam("woAvo") long woAvoDoc,
+                            @RequestParam("doAvo") long doAvoDoc,
+                            @RequestParam("vrAvo") long vrAvoDoc,
+                            Model model) {
+
+        Week week = weekRepository.findById(weekId);
+        Dag maandag = week.getDag("maandag");
+        Dag dinsdag = week.getDag("dinsdag");
+        Dag woensdag = week.getDag("woensdag");
+        Dag donderdag = week.getDag("donderdag");
+        Dag vrijdag = week.getDag("vrijdag");
+        System.out.println("wat is dit? " + maAvoDoc);
+
+        User user1 = userRepository.findUserById(maOchtDoc);
+        User user2 = userRepository.findUserById(maMidDoc);
+        User user3 = userRepository.findUserById(maAvoDoc);
+
+        User user4 = userRepository.findUserById(diOchtDoc);
+        User user5 = userRepository.findUserById(diMidDoc);
+        User user6 = userRepository.findUserById(diAvoDoc);
+        System.out.println("welke user is dit? " + user1);
+
+        User user7 = userRepository.findUserById(woOchtDoc);
+        User user8 = userRepository.findUserById(woMidDoc);
+        User user9 = userRepository.findUserById(woAvoDoc);
+
+        User user10 = userRepository.findUserById(doOchtDoc);
+        User user11 = userRepository.findUserById(doMidDoc);
+        User user12 = userRepository.findUserById(doAvoDoc);
+
+        User user13 = userRepository.findUserById(vrOchtDoc);
+        User user14 = userRepository.findUserById(vrMidDoc);
+        User user15 = userRepository.findUserById(vrAvoDoc);
+
+        maandag.getOchtend().setDocent(user1);
+        maandag.getMiddag().setDocent(user2);
+        maandag.getAvond().setDocent(user3);
+        dagRepository.save(maandag);
+        System.out.println("is dit maandag? " + maandag);
+
+        dinsdag.getOchtend().setDocent(user4);
+        dinsdag.getMiddag().setDocent(user5);
+        dinsdag.getAvond().setDocent(user6);
+        dagRepository.save(dinsdag);
+
+        woensdag.getOchtend().setDocent(user7);
+        woensdag.getMiddag().setDocent(user8);
+        woensdag.getAvond().setDocent(user9);
+        dagRepository.save(woensdag);
+
+        donderdag.getOchtend().setDocent(user10);
+        donderdag.getMiddag().setDocent(user11);
+        donderdag.getAvond().setDocent(user12);
+        dagRepository.save(donderdag);
+
+        vrijdag.getOchtend().setDocent(user13);
+        vrijdag.getMiddag().setDocent(user14);
+        vrijdag.getAvond().setDocent(user15);
+        dagRepository.save(vrijdag);
         return "roosteraar/docenten-koppelen-gekozen-cohort";
     }
 }
