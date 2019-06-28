41a5bb2da3e53911173e47147c0a9924037c80f1
linusband <linusband@gmail.com>
2019-06-28 15:57:02 +0200

    AlgBesController post-method teruggebracht naar 15rgl met Map. thx Seán

--- src/main/java/sqrt4/mijninzet/controller/AlgemeneBeschikbaarheidController.java
+++ src/main/java/sqrt4/mijninzet/controller/AlgemeneBeschikbaarheidController.java
@@ -12,14 +12,20 @@ import sqrt4.mijninzet.model.User;
 import sqrt4.mijninzet.repository.WeekRepository;
 
 import java.util.Map;
+import java.util.Set;
 
 @RequestMapping("/docent")
 @Controller
 public class AlgemeneBeschikbaarheidController extends AbstractController {
     public static final int NUMMER_ALGEMENE_WEEK = 0;
+    public static final int MAAK_MIDDAGINDEX = 5;
+    public static final int MAAK_AVONDINDEX = 10;
+
 
     @Autowired
     WeekRepository weekRepo;
+
+    public static final String[] DAGEN = {"maandag", "dinsdag", "woensdag", "donderdag", "vrijdag"};
 
     @GetMapping("/algemene-beschikbaarheid")
     public String AlgemeneBeschikbaarheid(Model model) {
@@ -45,42 +51,16 @@ public class AlgemeneBeschikbaarheidController extends AbstractController {
     }
 
     @PostMapping(value = "/algemene-beschikbaarheid-updaten")
-    public String updateAlgemeneBeschikbaarheid(@RequestParam Map<String, Boolean> allParams,
-                                                @RequestParam("maandagochtend") boolean maOBeschikbaar,
-                                                @RequestParam("maandagmiddag") boolean maMBeschikbaar,
-                                                @RequestParam("maandagavond") boolean maABeschikbaar,
-                                                @RequestParam("dinsdagochtend") boolean diOBeschikbaar,
-                                                @RequestParam("dinsdagmiddag") boolean diMBeschikbaar,
-                                                @RequestParam("dinsdagavond") boolean diABeschikbaar,
-                                                @RequestParam("woensdagochtend") boolean woOBeschikbaar,
-                                                @RequestParam("woensdagmiddag") boolean woMBeschikbaar,
-                                                @RequestParam("woensdagavond") boolean woABeschikbaar,
-                                                @RequestParam("donderdagochtend") boolean doOBeschikbaar,
-                                                @RequestParam("donderdagmiddag") boolean doMBeschikbaar,
-                                                @RequestParam("donderdagavond") boolean doABeschikbaar,
-                                                @RequestParam("vrijdagochtend") boolean vrOBeschikbaar,
-                                                @RequestParam("vrijdagmiddag") boolean vrMBeschikbaar,
-                                                @RequestParam("vrijdagavond") boolean vrABeschikbaar,
-                                                Model model) {
-
-        System.out.println(allParams);
+    public String updateAlgemeneBeschikbaarheid(@RequestParam Map<String, String> allParams, Model model) {
+        Set<String> keys = allParams.keySet();
+        String[] beschikbaarheden = keys.toArray(new String[keys.size()]);
         Week algemeneWeek = weekRepo.findByWeekNummerAndUser(NUMMER_ALGEMENE_WEEK,voegActiveUserToe());
 
-        algemeneWeek.getDag("maandag").getOchtend().setBeschikbaar(maOBeschikbaar);
-        algemeneWeek.getDag("maandag").getMiddag().setBeschikbaar(maMBeschikbaar);
-        algemeneWeek.getDag("maandag").getAvond().setBeschikbaar(maABeschikbaar);
-        algemeneWeek.getDag("dinsdag").getOchtend().setBeschikbaar(diOBeschikbaar);
-        algemeneWeek.getDag("dinsdag").getMiddag().setBeschikbaar(diMBeschikbaar);
-        algemeneWeek.getDag("dinsdag").getAvond().setBeschikbaar(diABeschikbaar);
-        algemeneWeek.getDag("woensdag").getOchtend().setBeschikbaar(woOBeschikbaar);
-        algemeneWeek.getDag("woensdag").getMiddag().setBeschikbaar(woMBeschikbaar);
-        algemeneWeek.getDag("woensdag").getAvond().setBeschikbaar(woABeschikbaar);
-        algemeneWeek.getDag("donderdag").getOchtend().setBeschikbaar(doOBeschikbaar);
-        algemeneWeek.getDag("donderdag").getMiddag().setBeschikbaar(doMBeschikbaar);
-        algemeneWeek.getDag("donderdag").getAvond().setBeschikbaar(doABeschikbaar);
-        algemeneWeek.getDag("vrijdag").getOchtend().setBeschikbaar(vrOBeschikbaar);
-        algemeneWeek.getDag("vrijdag").getMiddag().setBeschikbaar(vrMBeschikbaar);
-        algemeneWeek.getDag("vrijdag").getAvond().setBeschikbaar(vrABeschikbaar);
+        for (int i = 0; i < DAGEN.length; i++) {
+            algemeneWeek.getDag(DAGEN[i]).getOchtend().setBeschikbaar(Boolean.parseBoolean(allParams.get(beschikbaarheden[i])));
+            algemeneWeek.getDag(DAGEN[i]).getMiddag().setBeschikbaar(Boolean.parseBoolean(allParams.get(beschikbaarheden[i + MAAK_MIDDAGINDEX])));
+            algemeneWeek.getDag(DAGEN[i]).getAvond().setBeschikbaar(Boolean.parseBoolean(allParams.get(beschikbaarheden[i + MAAK_AVONDINDEX])));
+        }
 
         weekRepo.save(algemeneWeek);
         model.addAttribute("algemeneWeek", algemeneWeek);
